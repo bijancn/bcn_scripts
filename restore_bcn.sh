cb639e47a5ea6c7bff1cb1b4b3cbd8ecd18b5e17
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2014-03-09 23:37:44 +0100

    merging

--- restore_bcn.sh
+++ restore_bcn.sh
@@ -14,20 +14,20 @@ DIRS=".matplotlib .ssh texmf/tex/latex "
 DIRS+=".vim .vim/syntax .vim/spell .vim/colors .vim/ftplugin "
 DIRS+=".config/keepassx .config/terminator .config/inkscape .config/evince "
 DIRS+=".config/dwb .config/dwb/adblock_lists .config/dwb/default "
-DIRS+=".config/dconf/user"
+DIRS+=""
 for d in $DIRS
 do
   mkdir ~/$d -p
   ln -sf $bcn/$d/*                      ~/$d/
 done
 
-files=".vimrc .bashrc .coloritrc .dir_colorsrc .muttrc .gitignore_global"
+files=".vimrc .bashrc .coloritrc .dir_colorsrc .muttrc .gitignore_global .config/dconf/user"
 for f in $files
 do
   ln -sf $bcn/$f                      ~/$f
 done
 ln -sf $bcn/gitconf/.gitconfig$app      ~/.gitconfig
-if $mighty 
-then
-  sudo ln -sf $bcn/metacity-theme-1.xml /usr/share/themes/Mint-X/metacity-1/metacity-theme-1.xml
-fi
+#if $mighty 
+#then
+  #sudo ln -sf $bcn/metacity-theme-1.xml /usr/share/themes/Mint-X/metacity-1/metacity-theme-1.xml
+#fi
