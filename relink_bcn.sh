15ac500b7308fc934fba912cefeba48967c3765a
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2014-04-27 19:25:44 +0200

    progress in vim relinkage

--- relink_bcn.sh
+++ relink_bcn.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 # Move all directory-based to file based linkage
+shopt -s extglob
 
 if [ "$USER" = "bijancn" ]
 then
@@ -13,28 +14,30 @@ fi
 bcn=~/bcn_scripts
 safe=~/safe/keys
 DIRS=".matplotlib texmf/tex/latex "
-DIRS+=".vim .vim/syntax .vim/spell .vim/colors .vim/ftplugin "
 DIRS+=".config/keepassx .config/terminator .config/inkscape .config/evince "
 DIRS+=".config/dwb .config/dwb/adblock_lists .config/dwb/default "
 DIRS+=""
-for d in $DIRS
-do
-  #mkdir ~/$d -p
-  #ln -sf $bcn/$d/*                      ~/$d/
-done
 
-files=".vimrc .bashrc .coloritrc .dir_colorsrc .gitignore_global "
+files=".@(vim|bash|colorit|dir_colors)rc .gitignore_global "
+files+=".vim/@(bcn_color_demo|filetype).vim "
+files+=".vim/colors/@(bcn_dark|bcn_light|print_bw).vim "
+files+=".vim/ftplugin/@(c|markdown|noweb|ocaml|python).vim "
+files+=".vim/spell/en.utf-8.add?(.spl) "
+files+=".vim/syntax/@(markdown|noweb|sindarin).vim "
+files+=".vim/UltiSnips/@(all|fortran|pandoc|noweb|tex).snippets "
 files+=".ssh/config .ssh/known_hosts "
 files+=".local/share/applications/mimeapps.list "
 
 for f in $files; do
   mkdir ~/$(dirname $f) -p
+  echo $f
   if [ -f $bcn/$f ] ; then
     ln -sf $bcn/$f                      ~/$f
   elif [ -f $safe/$f ] ; then
     ln -sf $safe/$f                     ~/$f
   fi
 done
+
 ln -sf $bcn/gitconf/.gitconfig$app      ~/.gitconfig
 if $mighty; then
   sudo easy_install trash-cli
