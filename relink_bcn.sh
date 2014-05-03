b333b981999c5dc49d0f687ab672512bb5e5b1f2
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2014-05-03 11:37:23 +0200

    Adding `bin`.

--- relink_bcn.sh
+++ relink_bcn.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-# Move all directory-based to file based linkage
 shopt -s extglob
 
 if [ "$USER" = "bijancn" ]
@@ -13,28 +12,38 @@ fi
 
 bcn=~/bcn_scripts
 safe=~/safe/keys
-DIRS=".matplotlib texmf/tex/latex "
-DIRS+=".config/keepassx .config/terminator .config/inkscape .config/evince "
-DIRS+=".config/dwb .config/dwb/adblock_lists .config/dwb/default "
-DIRS+=""
+l="texmf/tex/latex"
 
 files=".@(vim|bash|colorit|dir_colors)rc .gitignore_global "
+files+=".config/evince/@(accels|print-settings) "
+files+=".config/keepassx/config.ini "
+files+=".config/terminator/config "
+files+=".local/share/applications/mimeapps.list "
+files+=".matplotlib/matplotlibrc "
+files+=".ssh/config .ssh/known_hosts "
 files+=".vim/@(bcn_color_demo|filetype).vim "
 files+=".vim/colors/@(bcn_dark|bcn_light|print_bw).vim "
 files+=".vim/ftplugin/@(c|markdown|noweb|ocaml|python).vim "
 files+=".vim/spell/en.utf-8.add?(.spl) "
 files+=".vim/syntax/@(markdown|noweb|sindarin).vim "
 files+=".vim/UltiSnips/@(all|fortran|pandoc|noweb|tex).snippets "
-files+=".ssh/config .ssh/known_hosts "
-files+=".local/share/applications/mimeapps.list "
+files+="bin/@(connect_free_server|gen_dirs_from_sins|handbreak_kill).sh "
+files+="bin/@(ping_scan.sh|ppdflatex|server_status.sh|show_my_jobs.sh) "
+files+="bin/@(svn-diff.sh|time.py) "
+files+="$l/bcn_beamer@(.sty|_example.pdf|_example.tex) "
+files+="$l/bcn_@(color|commands|koma).sty "
+files+="$l/bcn_letter@(.lco|_example.pdf|_example.tex) "
 
 for f in $files; do
   mkdir ~/$(dirname $f) -p
   echo $f
+  # Try to use public file. If not there use private.
   if [ -f $bcn/$f ] ; then
     ln -sf $bcn/$f                      ~/$f
   elif [ -f $safe/$f ] ; then
     ln -sf $safe/$f                     ~/$f
+  else
+    echo 'FILE NOT FOUND'
   fi
 done
 
