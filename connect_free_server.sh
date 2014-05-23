da6ca8c18b6ba76c1c24e0adccd7ccfce9a14da0
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2014-05-23 11:22:40 +0200

    allowing to skip some servers

--- connect_free_server.sh
+++ connect_free_server.sh
@@ -1,11 +1,20 @@
 #/bin/bash
 
+j=0
 for i in {22..01} ; do
   load=`ssh theoc$i uptime | awk '{print $12}'`
   echo theoc$i: average load during 15 min: ${load}
   load=${load:0:1}
+  if [ -z "$1" ]; then
+    toskip=0
+  else
+    toskip=$1
+  fi
   if [ ${load} = 0 ]; then
-    ssh theoc$i
-    break
+    if [ "$j" = "${toskip}" ]; then
+      ssh theoc$i
+      break
+    fi
+    j=$((j+1))
   fi
 done
