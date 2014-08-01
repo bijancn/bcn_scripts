82378b63ac2fbc6bf21d1de975d9f3c2317553b0
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2014-08-01 23:05:30 +0200

    Accumulating git setup stuff

---
+++ bin/setup_git.sh
@@ -0,0 +1,7 @@
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
+curl http://git.kernel.org/cgit/git/git.git/plain/contrib/completion/git-completion.bash > ~/.git-completion.sh
+cd $HOME
+github gsec eZchat
+github gsec glowing-mustache
+github oblique63 Python-GoogleCalendarParser
+github bijancn bcn_scripts
