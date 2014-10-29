363d3c2c0a379c7cbc183a1041cd508812f84506
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2014-10-29 16:25:19 +0100

    Refinements for DESY environment and more

--- bin/build_tool.sh
+++ bin/build_tool.sh
@@ -5,8 +5,8 @@ else
 fi
 echo "Base dir is $whiz"
 
-if [ $# -lt 3 ]; then
-  echo 'Usage: build_tool.sh build n k l'
+if [ $# -lt 4 ]; then
+  echo 'Usage: build_tool.sh build n k l tag'
   echo '- build = a -> all builds = def doc debug ifort'
   echo '- build = def -> default compiler and flags'
   echo '- build = debug -> gfortran with DEBUG flags'
@@ -22,13 +22,15 @@ if [ $# -lt 3 ]; then
   echo '- k = 1 -> make'
   echo '- l = 0 -> dont test'
   echo '- l = 1 -> perform make check'
+  echo '- tag -> if given, add to buildname: build-tag'
   exit
 fi
-if [ $1 == 'a'  ]; then
+if [ "$1" == "a" ]; then
   builds='def debug doc'
 else
-  if [ $1 == 'allomegas' ]; then
+  if [ "$1" == "allomegas" ]; then
     builds='ifomegaO0 gfomegaO0 ifomegaO1 gfomegaO1 ifomegaO2 gfomegaO2 ifomegaO3 gfomegaO3'
+    builds='ifomegaO2 gfomegaO2 ifomegaO3 gfomegaO3'
   else
     builds=$1
   fi
@@ -36,6 +38,11 @@ fi
 conf=$2
 make=$3
 check=$4
+if [ $# -eq 5 ]; then
+  tag='-'$5
+else
+  tag=''
+fi
 echo "Building $builds, confing $conf, making $make and checking $check"
 
 if [ -d $whiz ]; then
@@ -52,77 +59,78 @@ if [ $conf -eq 2 ]; then
 fi
 cd build
 for b in $builds; do
-  mkdir $b
-  cd $b
+  dir=$b$tag
+  mkdir $dir
+  cd $dir
   if (($conf > 0)); then
-    echo "Configuring $b"
+    echo "Configuring $b in $dir"
     case $b in
       doc)
-        $whiz/configure --prefix=$whiz/install/$b \
+        $whiz/configure --prefix=$whiz/install/$dir \
           --enable-distribution > /dev/null &
         ;;
 
       debug)
-        $whiz/configure --prefix=$whiz/install/$b FC=gfortran \
+        $whiz/configure --prefix=$whiz/install/$dir FC=gfortran \
           FCFLAGS="$DEBUG" > /dev/null &
         ;;
 
       def)
-        $whiz/configure --prefix=$whiz/install/$b > /dev/null &
+        $whiz/configure --prefix=$whiz/install/$dir > /dev/null &
         ;;
 
       ifomegaO0)
-        $whiz/omega/configure --prefix=$whiz/install/$b FC=ifort FCFLAGS=-O0 \
+        $whiz/omega/configure --prefix=$whiz/install/$dir FC=ifort FCFLAGS=-O0 \
           > /dev/null &
         ;;
 
       gfomegaO0)
-        $whiz/omega/configure --prefix=$whiz/install/$b FC=gfortran \
+        $whiz/omega/configure --prefix=$whiz/install/$dir FC=gfortran \
           FCFLAGS=-O0 > /dev/null &
         ;;
 
       ifomegaO1)
-        $whiz/omega/configure --prefix=$whiz/install/$b FC=ifort FCFLAGS=-O1 \
+        $whiz/omega/configure --prefix=$whiz/install/$dir FC=ifort FCFLAGS=-O1 \
           > /dev/null &
         ;;
 
       gfomegaO1)
-        $whiz/omega/configure --prefix=$whiz/install/$b FC=gfortran \
+        $whiz/omega/configure --prefix=$whiz/install/$dir FC=gfortran \
           FCFLAGS=-O1 > /dev/null &
         ;;
 
       ifomegaO2)
-        $whiz/omega/configure --prefix=$whiz/install/$b FC=ifort FCFLAGS=-O2 \
+        $whiz/omega/configure --prefix=$whiz/install/$dir FC=ifort FCFLAGS=-O2 \
           > /dev/null &
         ;;
 
       gfomegaO2)
-        $whiz/omega/configure --prefix=$whiz/install/$b FC=gfortran \
+        $whiz/omega/configure --prefix=$whiz/install/$dir FC=gfortran \
           FCFLAGS=-O2 > /dev/null &
         ;;
 
       ifomegaO3)
-        $whiz/omega/configure --prefix=$whiz/install/$b FC=ifort FCFLAGS=-O3 \
+        $whiz/omega/configure --prefix=$whiz/install/$dir FC=ifort FCFLAGS=-O3 \
           > /dev/null &
         ;;
 
       gfomegaO3)
-        $whiz/omega/configure --prefix=$whiz/install/$b FC=gfortran \
+        $whiz/omega/configure --prefix=$whiz/install/$dir FC=gfortran \
           FCFLAGS=-O3 > /dev/null &
         ;;
 
       gfopenmp)
-        $whiz/configure --prefix=$whiz/install/$b FC=gfortran FCFLAGS='-O2' \
+        $whiz/configure --prefix=$whiz/install/$dir FC=gfortran FCFLAGS='-O2' \
           --enable-fc-openmp > /dev/null &
         ;;
 
       gfomegaopenmp)
-        $whiz/omega/configure --prefix=$whiz/install/$b FC=gfortran FCFLAGS='-O2' \
+        $whiz/omega/configure --prefix=$whiz/install/$dir FC=gfortran FCFLAGS='-O2' \
           --enable-fc-openmp > /dev/null &
         ;;
 
       ifomegaopenmp)
-        $whiz/omega/configure --prefix=$whiz/install/$b FC=ifort \
+        $whiz/omega/configure --prefix=$whiz/install/$dir FC=ifort \
           FCFLAGS='-O2 -openmp' --enable-fc-openmp > /dev/null &
 
     esac
@@ -132,8 +140,8 @@ done
 wait
 echo "========= CONFIGURE IS DONE ========="
 for b in $builds; do
-  cd $b
-  echo "Building and installing $b"
+  cd $b$tag
+  echo "Building and installing $b$tag"
   if [ $make -eq 1 ]; then
     (make -j 2 --silent; make install) &
   fi
@@ -142,8 +150,8 @@ done
 wait
 echo "========= MAKE IS DONE ========="
 for b in $builds; do
-  cd $b
-  echo "Checking $b"
+  cd $b$tag
+  echo "Checking $b$tag"
   if [ $check -eq 1 ]; then
     make check &
   fi
