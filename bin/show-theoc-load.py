3f26ffe80eb3a1e963231638134ac3df4f9578fb
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2016-03-18 18:46:09 +0100

    Make show-theo-load epic

--- bin/show-theoc-load.py
+++ bin/show-theoc-load.py
@@ -1,14 +1,56 @@
 #!/usr/bin/env python
-import subprocess, re
+import subprocess, re, collections
+from termcolor import colored
 
 re_l = re.compile("([0-9]+\.[0-9]+)$")
-for i in range(25,1,-1):
-  machine = "theoc%02d" % (i + 1)
+total_cores = 0
+total_load = 0.0
+for i in range(36,0,-1):
+  machine = "theoc%02d" % (i)
   node = "bcho@" + machine
-  ret = subprocess.check_output(["ssh", node, "uptime &&", "who"]);
+  ret = subprocess.check_output(["ssh", node, "uptime &&", "who &&",
+    "ps -eo pcpu,user | sed -e 's/^[[:space:]]*//' | sort -k1 -r | head -41 |tail -40 &&",
+    "grep '^core id' /proc/cpuinfo | sort -u | wc -l &&",
+    "echo $(($(grep \"^physical id\" /proc/cpuinfo | awk \'{print $4}\' | sort -un | tail -1)+1))"]);
   lines = ret.split('\n')
+  if lines.pop() != '':
+    print "ALERT THIS SHOULD HAVE BEEN EMPTY"
+  real_cores = int(lines.pop()) * int(lines.pop())
+  total_cores += real_cores
+  load15 = float(re_l.search(lines.pop(0)).group(1))
+  total_load += min(load15, real_cores)
   users = []
-  for line in lines[1:-1]:
+  for line in lines[:-40]:
     users += [line.split(' ')[0]]
-  load15 = re_l.search(lines[0]).group(1)
-  print machine, "\tload(15min): ", load15, "\t logged in: ", ' '.join(users)
+  if len(users) > 0:
+    users_strg = ','.join(set(users))
+  else:
+    users_strg = '[]'
+
+  if load15 / real_cores > 0.5:
+    color = 'red'
+  elif load15 / real_cores < 0.05:
+    color = 'green'
+  else:
+    color = 'blue'
+
+  top_processes = filter(lambda line: float(line.split(' ')[0]) > 10, lines[-40:])
+  top_usage = {}
+  for line in top_processes:
+    usage = float(line.split(' ')[0])
+    user  = line.split(' ')[1]
+    tmp = {user: usage}
+    try:
+      top_usage[user] += usage
+    except KeyError:
+      top_usage.update(tmp)
+  usage_strg = ''
+  for user in top_usage:
+    usage_strg += user + ':%3.1f' % (top_usage[user]/100) + ' '
+
+  string = machine + " load15: %4.1f" % load15 + '  cores: %2i' % real_cores + \
+      "  loggedin:%20s" % users_strg + '  usage: ' + usage_strg
+  print colored(string, color)
+print "="*80
+print "total number of cores:" + str(total_cores) + " loaded with " + \
+    str(total_load) + " (%4.1f%)" % (total_load / total_cores * 100)
