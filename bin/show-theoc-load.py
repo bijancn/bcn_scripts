36c3b2fbff8d514e6fca1ea6ad57ca8e2760534f
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2016-03-18 16:53:51 +0100

    Fix show-theoc-load.py, also show users and remove bash version

--- bin/show-theoc-load.py
+++ bin/show-theoc-load.py
@@ -1,11 +1,14 @@
-#/usr/bin/python
+#!/usr/bin/env python
 import subprocess, re
 
-re_u = re.compile("([0-9]+) users?")
 re_l = re.compile("([0-9]+\.[0-9]+)$")
-for i in range(25):
-  node = "bcho@theoc%02d" % (i + 1)
-  ret = subprocess.check_output(["ssh", node, "uptime"]);
-  users = re_u.search(ret).group(1)
-  load15 = re_l.search(ret).group(1)
-  print node, "\t load(15min): ", load15, "\t users: ", users
+for i in range(25,1,-1):
+  machine = "theoc%02d" % (i + 1)
+  node = "bcho@" + machine
+  ret = subprocess.check_output(["ssh", node, "uptime &&", "who"]);
+  lines = ret.split('\n')
+  users = []
+  for line in lines[1:-1]:
+    users += [line.split(' ')[0]]
+  load15 = re_l.search(lines[0]).group(1)
+  print machine, "\tload(15min): ", load15, "\t logged in: ", ' '.join(users)
--- bin/show-theoc-load.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-j=0
-for i in {26..01} ; do
-  userload=`ssh bcho@theoc$i uptime | awk '{print "users:", $6, "\tload(15min):", $12}'`
-  echo -e "theoc$i \t ${userload}"
-done
