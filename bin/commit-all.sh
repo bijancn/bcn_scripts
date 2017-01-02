c8885b2388340614a5ea3a416ff22349c145ed8e
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2017-01-02 18:16:17 +0100

    Automatic commit on 2017-01-02

--- bin/commit-all.sh
+++ bin/commit-all.sh
@@ -1,5 +1,7 @@
 DIRS="~/safe/phd/thesis ~/vimwiki/"
 for dir in $DIRS; do
+  echo "-------------- Updating $dir --------------"
+  cd $dir
   git add .
   git commit -m "Automatic commit on `date +%F`"
   git push
