b75399e8ac07da17e82ccaa919ccb9c68edebb3c
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2017-03-27 11:48:35 +0200

    Add resubmitter script

---
+++ bin/resubmit-not-done.sh
@@ -0,0 +1,15 @@
+for i in *$1*/ ; do
+  # TODO: (bcn 2017-02-21) combine with or
+  if ! grep 'event sample complete' $i/whizard.log; then
+  # if ! grep -q 'RES' $i/whizard.log; then
+    echo "Not complete in $i"
+    cd $i
+    # sed -i 's/n_events = .*$/n_events = 3000000/' *sin
+    qsub ../submit
+    rm done
+    cd -
+  else
+    echo "complete in $i"
+    touch $i/done
+  fi
+done
